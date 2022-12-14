a02a1f182849158301cf2a86d8c0c6d490f15c73
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2022-12-14 17:25:04 -0500

    Simplify 'foreign' metadata (#2845)

--- purs.nix
+++ purs.nix
@@ -1,5 +1,3 @@
-{ pkgs, npmlock2nix, ... }:
-
 {
   name = "timezone";
   srcs = [ "src" ];
@@ -10,8 +8,10 @@
     "cn-debug"
     "pre"
   ];
-  foreign.TimeZone.node_modules =
-    (npmlock2nix.node_modules { src = ./.; }) + /node_modules;
+  foreign.TimeZone = {
+    type = "npm";
+    path = ./.;
+  };
   test-dependencies = [
     "test-utils"
   ];
