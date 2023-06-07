923146e7f466c99c70367ee7c588eb31fa47ac47
Dave Zuch <dzuch@citizennet.com>
2023-06-06 17:27:04 -0700

    Remove unused Nix code
    The initiative to move our build system to Nix was never completed, and
    now it's certain it never will be. The unfinished work can sometimes
    make it so that making a minor change or just switching branches can
    cause everything to rebuild, which can take a hugely disruptive amount
    of time. That's a high cost to pay for something we don't use, so we get
    rid of it.

--- default.nix
+++
@@ -1,8 +0,0 @@
-{ self, ... }: {
-  perSystem = { config, ... }: {
-    packages.timezone = config.purs-nix-build ./.;
-    checks.timezone-test =
-      let ps = config.packages.array.purs-nix-info-extra.ps;
-      in ps.test.check { };
-  };
-}
